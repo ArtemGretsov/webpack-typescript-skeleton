8046a1771ca3d023ef15fea66f2370488d2559cb
Artem Gretsov <agreczov@gmail.com>
2021-05-05 00:28:23 +0300

    Init skeleton webpack project (typescript)

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const example = () => {
+  // eslint-disable-next-line no-console
+  alert('example')
+}
+
+example()
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+    ],
+};
